2c3b182030dc8025d522d6eeb4ff92da5dfcc28f
najachen <najachen@gmail.com>
2016-08-26 14:15:56 +0800

    Initial commit

---
+++ inventory/src/main/java/silver/reminder/inventory/InventoryFinanceActivity.java
@@ -0,0 +1,21 @@
+package silver.reminder.inventory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class InventoryFinanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finance);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
