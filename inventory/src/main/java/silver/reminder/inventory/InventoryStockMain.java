0f5b8b9de4ddfb96388191afb8e4c19cf137805e
najachen <najachen@gmail.com>
2016-08-30 12:00:27 +0800

    naja v1

--- inventory/src/main/java/silver/reminder/inventory/InventoryStockMain.java
+++ inventory/src/main/java/silver/reminder/inventory/InventoryStockMain.java
@@ -4,22 +4,37 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class InventoryStockMain extends AppCompatActivity {
 
     private ListView list;
+    private TextView l_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         findViews();
+        l_title.setText("庫存");
+        selectData();
+    }
+
+    private void selectData() {
         InventoryMyDBHelper helper = new InventoryMyDBHelper(this, "inventory.db", null, 1);
         Cursor c = helper.getReadableDatabase().rawQuery("select * from inventory join item " +
                 "on inventory.item_id = item._id  and qty <> -1", new String[]{});
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                R.layout.activity_col2_stock,
+                c,
+                new String[]{"itemName", "qty"},
+                new int[]{R.id.t_row_stock_itemName, R.id.t_row_stock_qty},
+                0);
     }
 
     private void findViews() {
         list = (ListView) findViewById(R.id.list_list);
+        l_title = (TextView) findViewById(R.id.l_title);
     }
 }
